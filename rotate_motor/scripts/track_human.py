be9ad5b9f310ef72bdf5a49b4b06f74082ccfea4
JashanSFU <jashanrajj@gmail.com>
2024-04-26 12:54:37 -0700

    changes logic for rotation

--- rotate_motor/scripts/track_human.py
+++ rotate_motor/scripts/track_human.py
@@ -6,7 +6,13 @@ from zed_interfaces.msg import ObjectsStamped
 from geometry_msgs.msg import PoseStamped , PoseArray
 from dynamixel_workbench_msgs.srv import *
 from rospy_tutorials.srv import *
+from camera_dimensions import get_camera_dimensions
 
+# Get camera dimensions
+width, height = get_camera_dimensions()
+
+# Use camera dimensions
+# print("Camera dimensions: {}x{}".format(width, height))
 
 class human_traj_prediction():
     def __init__(self):
@@ -21,9 +27,15 @@ class human_traj_prediction():
         self.deg_to_res = 1024/90
         self.goal = 60
         self.count = -1
+        self.last_detection_direction = 'none'
 
-        self.p_x_filt =[]
-        self.p_y_filt =[]
+        # the camera has a vision of 110 degrees
+        # lets say the human is on the very edge, that is at 110 degree
+        # the camera has to rotate 55 degrees to brings human to centre. Thus, max degree
+        self.max_rotation_angle = 55 
+
+        # self.p_x_filt =[]
+        # self.p_y_filt =[]
 
 
     def send_goal(self):
@@ -48,74 +60,56 @@ class human_traj_prediction():
         for obj in data.objects:
             if obj.label != 'Person':
                 continue
-        
-            feat = {'x':obj.position[0],
-                 'y':obj.position[1],
-                 'bb_x_min': obj.bounding_box_2d.corners[0].kp[0],
-                 'bb_x_max': obj.bounding_box_2d.corners[2].kp[0],
-                 'bb_y_min': obj.bounding_box_2d.corners[0].kp[1],
-                 'bb_y_max': obj.bounding_box_2d.corners[2].kp[1]}
+            else:
+                feat = {'x':obj.position[0],
+                    'y':obj.position[1],
+                    'bb_x_min': obj.bounding_box_2d.corners[0].kp[0],
+                    'bb_x_max': obj.bounding_box_2d.corners[2].kp[0],
+                    'bb_y_min': obj.bounding_box_2d.corners[0].kp[1],
+                    'bb_y_max': obj.bounding_box_2d.corners[2].kp[1]}
 
         return feat
 
     def predictions_callback(self,data):
         self.count += 1
 
+        # slow down using exhaustive wait ( do we really need it )
+        feat = self.human_data(data)
+        if(feat != {}):
+            self.p_x_bounds.append((feat['bb_x_min'],feat['bb_x_max']))
+            self.last_detection_direction = 'left' if feat['x'] < width/2 else 'right'
+
+        #######  person is not detected
+        else:
+            self.p_x_bounds.append((0,0))
+
+            # detection at edges is not correct so we will try to make smaller turns but 5 turns to cover entire 360
+            # avg turn to cover 360 degree and cover most visuals in center pixels (62 degree ?? )
+            print("Looking for the human")
+            if(self.last_detection_direction == 'left' )
+                self.goal -= 62
+            else 
+                self.goal += 62
+            self.send_goal()
+
         if self.count % 5 == 0:
-            feat = self.human_data(data)
-            # print(feat)
 
-            #######  person is not detected
-            if feat == {}:
-                print("Looking for the human")
-                self.goal += 120 
-                self.send_goal()
+            median_pair = statistics.median(self.p_x_bounds)
+            median_bb_x_min, median_bb_x_max = median_pair 
 
-
+                
             ###### person is detected
             if len(feat):
 
                 ###### To keep the human in the middle of the image
-    
-                    mean_bb = (feat['bb_x_min'] + feat['bb_x_max']) /2
-                    # print("mean_bb: ", mean_bb)
-                    turn = 30
-                    
-                    if mean_bb < 350:
-                        self.goal += turn
-                        print("rotating ", turn , " deg")
-                        self.send_goal()   
-
-                    elif mean_bb > 650:
-                        self.goal -= turn
-                        print("rotating -", turn , " deg")
-                        self.send_goal() 
-
-
-            # ########### filtering human's pose
-            # self.p_x_filt.append(feat['x'])
-            # self.p_y_filt.append(feat['y'])
-
-            # # to filter the noise
-            # if len(self.p_x_filt) > 3:
-            #     self.p_x_filt.pop(0)
-            #     self.p_y_filt.pop(0)
-
-            # x = np.mean(self.p_x_filt)    
-            # y = np.mean(self.p_y_filt) 
-
-            # poseArray = PoseArray()
-            # poseArray.header.frame_id = 'camera'
-            # poseArray.header.stamp = rospy.Time.now()
-
-            # #publishing current and filtered pose of the person
-            # pose = PoseStamped()
-            # pose.header.frame_id = 'camera'
-            # pose.header.stamp = rospy.Time.now()
-            # pose.pose.position.x = x
-            # pose.pose.position.y = y
-            # pose.pose.position.z = 0
-            # self.pub_cur_pose.publish(pose)
+                mean_bb = (abs(median_bb_x_min) + abs(median_bb_x_max)) /2
+                
+                # Calculate rotation angle based on mean position
+                turn = (mean_bb - width / 2) / (width / 2) * max_rotation_angle
+                
+                self.goal += turn
+                print("rotating -", turn , " deg")
+                self.send_goal() 
 
         # self.rate.sleep()
 
