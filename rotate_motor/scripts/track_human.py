21e125cc8829ef28ab5f43ee4d5af5f1172a0fec
Luna Sang <luna.sang21@gmail.com>
2024-07-08 15:38:53 -0700

    added position transformation and pubisher

--- rotate_motor/scripts/track_human.py
+++ rotate_motor/scripts/track_human.py
@@ -4,7 +4,8 @@ import statistics
 import rospy
 import numpy as np
 from zed_interfaces.msg import ObjectsStamped
-from geometry_msgs.msg import PoseStamped , PoseArray
+from geometry_msgs.msg import PoseStamped , PoseArray, PointStamped
+from nav_msgs.msg import Odometry
 from dynamixel_workbench_msgs.srv import *
 from rospy_tutorials.srv import *
 import sys
@@ -23,17 +24,24 @@ class human_traj_prediction():
         rospy.Subscriber('/zed2/zed_node/obj_det/objects', ObjectsStamped, self.predictions_callback)
         print('Subscriber setup for /zed2/zed_node/obj_det/objects')
 
+        rospy.Subscriber('odom', Odometry, self.robot_position_callback)
+
         self.pub_cur_pose = rospy.Publisher('/person_pose', PoseStamped, queue_size = 1)
         print('Publisher setup for /person_pose')
 
         self.pub_pred_pose_all = rospy.Publisher('/person_pose_pred_all', PoseArray, queue_size = 1)
         print('Publisher setup for /person_pose_pred_all')
 
+        self.pub_glob_coords = rospy.Publisher('/pub_glob_coords', PointStamped, queue_size=1)
+        print('Publisher setup for /pub_glob_coords')
+
 
         self.deg_to_res = 1024/90
         self.goal = 0
         self.count = -1
         self.notfound = 0
+        self.robot_position = [0,0,0]
+        self.robot_orientation = np.eye(3)
         print('Initialization complete')
 
     def send_goal(self):
@@ -63,10 +71,29 @@ class human_traj_prediction():
                     'bb_x_min': obj.bounding_box_2d.corners[0].kp[0],
                     'bb_x_max': obj.bounding_box_2d.corners[2].kp[0],
                     'bb_y_min': obj.bounding_box_2d.corners[0].kp[1],
-                    'bb_y_max': obj.bounding_box_2d.corners[2].kp[1]}
+                    'bb_y_max': obj.bounding_box_2d.corners[2].kp[1],
+                    'position': obj.position
+                    }
 
         return feat
 
+    ###### To get the global coordinates published by the robot
+    def robot_position_callback(self, data):
+        self.robot_position = np.array([data.pose.pose.position.x, data.pose.pose.position.y, data.pose.pose.position.z])
+
+        q = data.pose.pose.orientation
+        self.robot_orientation = self.quaternion_to_rotation_matrix(q)
+
+    def quaternion_to_rotation_matrix(self, q):
+        w, x, y, z = q.w, q.x, q.y, q.z
+        return np.array([[1 - 2*y**2 - 2*z**2, 2*x*y - 2*z*w, 2*x*z + 2*y*w],
+                         [2*x*y + 2*z*w, 1 - 2*x**2 - 2*z**2, 2*y*z - 2*x*w],
+                         [2*x*z - 2*y*w, 2*y*z + 2*x*w, 1 - 2*x**2 - 2*y**2]])
+    
+    def transform_to_global(self, local_coords):
+        local_coords = np.array(local_coords)
+        global_coords = np.dot(self.robot_orientation, local_coords) + self.robot_position
+        return global_coords
 
     def predictions_callback(self,data):
         self.count += 1
@@ -114,6 +141,19 @@ class human_traj_prediction():
                         print("rotating -", turn , " deg")
                         self.send_goal() 
 
+
+                    # transform the human position to global coordinates
+                    human_position_global = self.transform_to_global(feat['position'])
+                    print("Human position in global coordinates: ", human_position_global)
+
+                    # Publish the current human position
+                    point = PointStamped()
+                    point.header.stamp = rospy.Time.now()
+                    point.header.frame_id = 'map'
+                    point.point.x = human_position_global[0]
+                    point.point.y = human_position_global[1]
+                    point.point.z = human_position_global[2]
+                    self.pub_glob_coords.publish(point)
 
 if __name__ == '__main__':
     print("Starting the human trejectory prediction node")
