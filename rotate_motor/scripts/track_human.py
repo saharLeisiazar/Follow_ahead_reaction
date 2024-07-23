26fa13f66817611b85bba89ae502d6f290dd34c1
Luna Sang <luna.sang21@gmail.com>
2024-07-23 16:53:00 -0700

    update

--- rotate_motor/scripts/track_human.py
+++ rotate_motor/scripts/track_human.py
@@ -11,6 +11,7 @@ from nav_msgs.msg import Odometry
 from dynamixel_workbench_msgs.srv import *
 from rospy_tutorials.srv import *
 import sys
+from scipy.spatial.transform import Rotation
 # sys.path.append('/home/sahar/catkin_ws/src/Follow_ahead_reaction/rotate_motor/scripts/')
 # from camera_dimensions import get_camera_dimensions
 
@@ -45,26 +46,16 @@ class human_traj_prediction():
         self.count = -1
         self.notfound = 0
         self.robot_position = [0,0,0]
-        self.robot_orientation = np.eye(3)
-        self.motor_rotation_angle = 0.0
+        self.human_x = []
+        self.human_y = []
+        # self.robot_orientation = np.eye(3)
+        # self.motor_rotation_angle = 0.0
         
         #For testing
         self.human_global_pos = []
 
         print('Initialization complete')
 
-    # def get_current_motor_angle(self):
-    #     rospy.wait_for_service('/dynamixel_workbench/dynamixel_command')
-    #     try:
-    #         req = rospy.ServiceProxy('/dynamixel_workbench/dynamixel_command', DynamixelCommand)
-    #         response = req('',1,'Present_Position',2)
-    #         current_position = response.value
-    #         current_angle = current_position / self.deg_to_res
-    #         print(f'Current motor angle: {current_angle} degrees')
-    #         return math.radians(current_angle)
-    #     except rospy.ServiceException as e:
-    #         print("Service call failed: %s"%e)
-    #         return 0.0
 
     def send_goal(self):
 
@@ -104,88 +95,108 @@ class human_traj_prediction():
         self.robot_position = np.array([data.pose.pose.position.x, data.pose.pose.position.y, data.pose.pose.position.z])
 
         q = data.pose.pose.orientation
-        self.robot_orientation = self.quaternion_to_rotation_matrix(q)
+        rot = Rotation.from_quat([q.x, q.y, q.z, q.w])
+        rot_euler = rot.as_euler('xyz', degrees=False)
+        self.robot_orientation = rot_euler[2]
 
-    def quaternion_to_rotation_matrix(self, q):
-        w, x, y, z = q.w, q.x, q.y, q.z
-        return np.array([[1 - 2*y**2 - 2*z**2, 2*x*y - 2*z*w, 2*x*z + 2*y*w],
-                         [2*x*y + 2*z*w, 1 - 2*x**2 - 2*z**2, 2*y*z - 2*x*w],
-                         [2*x*z - 2*y*w, 2*y*z + 2*x*w, 1 - 2*x**2 - 2*y**2]])
-    
     def transform_to_global(self, local_coords):
         local_coords = np.array(local_coords)
-        # rorationmatix for the motor
-        # motor_rotation_angle = self.get_current_motor_angle()
-        motor_rotation_matrix = np.array([[math.cos(self.motor_rotation_angle), -math.sin(self.motor_rotation_angle), 0],
-                                           [math.sin(self.motor_rotation_angle), math.cos(self.motor_rotation_angle), 0],
+        angle = np.deg2rad(self.goal)
+        motor_rotation_matrix = np.array([[np.cos(angle), -np.sin(angle), 0],
+                                           [np.sin(angle), np.cos(angle), 0],
                                            [0, 0, 1]])
-        rotated_local_coords = np.dot(motor_rotation_matrix, local_coords)
-        global_coords = np.dot(self.robot_orientation, rotated_local_coords) + self.robot_position
-        return global_coords
+        global_coords = np.dot(motor_rotation_matrix, local_coords) + self.robot_position
+
+        global_coords = global_coords[:2]
+        global_ori = self.get_global_ori(global_coords)
+        return np.append(global_coords, global_ori)
+
+
+    def get_global_ori(self, coords):
+        self.human_x.append(coords[0])
+        self.human_y.append(coords[1])
+
+        if len(self.human_x > 10):
+            self.human_x.pop(0)
+            self.human_y.pop(0)
+
+        t = np.arange(len(self.human_x)) #.reshape(-1, 1)
+        x = np.array(self.human_x)
+        y = np.array(self.human_x)
+
+        coef_x = np.polyfit(t, x, 1)
+        coef_y = np.polyfit(t, y, 1)
+        p_x = np.poly1d(coef_x)
+        p_y = np.poly1d(coef_y)
+
+        new_x = p_x(t[-1]+ 5)
+        new_y = p_y(t[-1]+ 5)
+        global_ori = np.arctan2(new_y-y[-1], new_x-x[-1])
+
+        return global_ori
+
 
     def predictions_callback(self,data):
         self.count += 1
+        feat = self.human_data(data)
 
-        if self.count % 3 == 0:
-            feat = self.human_data(data)
-
-            ###### person is not detected            
-            if feat == {}:
-                print("Looking for the human ...")
-                self.notfound += 1
-                print("Not found count: ", self.notfound)
-                self.goal = (self.goal + 20) % 370
-                self.send_goal()
-                # rospy.sleep(0.3)
+        ###### person is not detected            
+        if feat == {} and self.count % 5 == 0:
+            print("Looking for the human ...")
+            self.notfound += 1
+            print("Not found count: ", self.notfound)
+            self.goal = (self.goal + 20) 
+            self.send_goal()
+            # rospy.sleep(0.3)
 
 
-            ###### person is detected
-            if len(feat):
-                    print("Person detected at x: ", feat['x'])
-                ###### To keep the human in the middle of the image
-    
-                    mean_bb = (feat['bb_x_min'] + feat['bb_x_max']) /2
-                    print("mean_bb: ", mean_bb)
-                    # turn = 10
-                    
-                    # Define center threshold based on a narrower range
-                    center_range_percentage = 0.15  # % on either side of the center
-                    image_center = 1280 / 2
-                    center_threshold_min = image_center * (1 - center_range_percentage)  # 1280 * (1-0.15)/2 = 576 
-                    center_threshold_max = image_center * (1 + center_range_percentage)  # 1280 * (1+0.15)/2 = 704
+        ###### person is detected
+        if len(feat) and self.count % 3 == 0:
+                # print("Person detected at x: ", feat['x'])
+            ###### To keep the human in the middle of the image
+
+                mean_bb = (feat['bb_x_min'] + feat['bb_x_max']) /2
+                # print("mean_bb: ", mean_bb)
+                # turn = 10
+                
+                # Define center threshold based on a narrower range
+                center_range_percentage = 0.15  # % on either side of the center
+                image_center = 1280 / 2
+                center_threshold_min = image_center * (1 - center_range_percentage)  # 1280 * (1-0.15)/2 = 576 
+                center_threshold_max = image_center * (1 + center_range_percentage)  # 1280 * (1+0.15)/2 = 704
 
 
-                    if mean_bb < center_threshold_min:
-                        turn = min(10, center_threshold_min - mean_bb)
-                        self.goal += turn
-                        print("rotating ", turn , " deg")
-                        self.send_goal()   
+                if mean_bb < center_threshold_min:
+                    turn = min(10, center_threshold_min - mean_bb)
+                    self.goal += turn
+                    print("rotating ", turn , " deg")
+                    self.send_goal()   
 
-                    elif mean_bb > center_threshold_max:
-                        turn = min(10, mean_bb - center_threshold_max)
-                        self.goal -= turn
-                        print("rotating -", turn , " deg")
-                        self.send_goal() 
+                elif mean_bb > center_threshold_max:
+                    turn = min(10, mean_bb - center_threshold_max)
+                    self.goal -= turn
+                    print("rotating -", turn , " deg")
+                    self.send_goal() 
 
 
-                    # transform the human position to global coordinates
-                    human_position_global = self.transform_to_global(feat['position'])
-                    print("Human position in global coordinates: ", human_position_global)
+                # transform the human position to global coordinates
+                human_position_global = self.transform_to_global(feat['position'])
+                # print("Human position in global coordinates: ", human_position_global)
 
-                    # for testing: store human pos for plotting and testing
-                    self.human_global_pos.append(human_position_global.tolist())
-                    # print("current global position shage: ", np.array(self.human_global_pos).shape)
-                    self.save_positions()
+                # for testing: store human pos for plotting and testing
+                # self.human_global_pos.append(human_position_global.tolist())
+                # # print("current global position shage: ", np.array(self.human_global_pos).shape)
+                # self.save_positions()
 
-                    # Publish the current human position
-                    point = PointStamped()
-                    point.header.stamp = rospy.Time.now()
-                    point.header.frame_id = 'map'
-                    point.point.x = human_position_global[0]
-                    point.point.y = human_position_global[1]
-                    point.point.z = human_position_global[2]
-                    self.pub_glob_coords.publish(point)
-    
+                # Publish the current human position
+                point = PointStamped()
+                point.header.stamp = rospy.Time.now()
+                point.header.frame_id = 'map'
+                point.point.x = human_position_global[0]
+                point.point.y = human_position_global[1]
+                point.point.z = human_position_global[2]
+                self.pub_glob_coords.publish(point)
+
     #For testing
     def save_positions (self):
         np.save(save_file, np.array(self.human_global_pos))
@@ -216,4 +227,4 @@ if __name__ == '__main__':
     human_traj_prediction()
     rospy.spin()   
 
-    human_traj_prediction().plot_human_positions() 
+    # human_traj_prediction().plot_human_positions() 
