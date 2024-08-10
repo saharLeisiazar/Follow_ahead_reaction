3433706de6054cb5a3ccb0224df6dc193373f6a7
Sahar <sahar.leysiazar@gmail.com>
2024-08-10 13:18:17 -0700

    update

--- follow/scripts/bar_plot.py
+++ follow/scripts/bar_plot.py
@@ -5,10 +5,11 @@ import matplotlib.pyplot as plt
 
 import matplotlib
 
+time = 12
 fig, axs = plt.subplots(1)
-colors = [colorsys.hsv_to_rgb(h, 1, 1) for h in np.linspace(0, 1, 13)]
+colors = [colorsys.hsv_to_rgb(h, 1, 1) for h in np.linspace(0, 1, time)]
 cmap = matplotlib.colors.ListedColormap(colors)
-pcm = axs.pcolormesh(np.random.random((1, 13)), cmap=cmap, vmin=0, vmax=13)
+pcm = axs.pcolormesh(np.random.random((1, time)), cmap=cmap, vmin=0, vmax=time)
 
 fig.colorbar(pcm, ax=axs, label='Time (s)')
 
